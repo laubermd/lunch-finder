2be484bd2dbea543f0aec36bcad6f7d7a26f3a41
Dylan Lauber <laubermd@gmail.com>
2017-01-29 02:39:59 -0500

    displays markers for near by lunch places

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -23,7 +23,6 @@ export class MapPage {
   }
 
   loadMap(){
-    //TODO loading screen while waiting for latLng
     Geolocation.getCurrentPosition().then((position) => {
 
       this.loaded = true;
@@ -37,8 +36,38 @@ export class MapPage {
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
+      let service = new google.maps.places.PlacesService(this.map);
+      // TODO move radius to Settings
+      service.nearbySearch({
+        location: latLng,
+        radius: 500,
+        type: ['restaurant']
+      },(results, status) => {this.placesCallback(results, status)});
     }, (err) => {
       console.log(err);
     });
+  }
+
+  createMarker(place){
+    let placeLoc = place.geometry.location;
+    let marker = new google.maps.Marker({
+      map: this.map,
+      position: place.geometry.location
+    });
+
+    // TODO display popup info / make click interactive
+    google.maps.event.addListener(marker, 'click', function() {
+      console.log("place",place);
+      //infowindow.setContent(place.name);
+      //infowindow.open(this.map, this);
+    });
+  }
+
+  placesCallback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        this.createMarker(results[i]);
+      }
+    }
   }
 }
